b72deb09547c262c667c0e74fec36994d652dd1b
axiever-divyesh <divyesh@axiever.com>
2023-09-28 21:10:19 +0530

    update

--- resources/views/pdf_merger.blade.php
+++ resources/views/pdf_merger.blade.php
@@ -13,6 +13,7 @@
         }
     </style>
 </head>
+
 <body>
     <h2>Upload and Merge PDF Files</h2>
 
@@ -34,20 +35,22 @@
         <div class="form-container">
             <!-- Initial set of form fields -->
             <div class="file-detailss">
-            <div class="file-details">
-                <label for="pdf_file_0">File Name:</label>
-                <input type="file" name="pdf_file[0]" id="pdf_file_0" class="pdf-file" accept=".pdf" required>
+                <div class="file-details">
+                    <label for="pdf_file_0">File Name:</label>
+                    <input type="file" name="pdf_file[0]" id="pdf_file_0" class="pdf-file" accept=".pdf" required>
 
-                <label for="merge_order_0">Merging Order:</label>
-                <input type="text" class="merge-order-input" name="merge_order[]" id="merge_order_0" value="1">
+                    <label for="merge_order_0">Merging Order:</label>
+                    <input type="text" class="merge-order-input" name="merge_order[]" id="merge_order_0"
+                        value="1">
 
-                <label for="slide_numbers_0">Slide Numbers:</label>
-                <input type="text" class="slide-numbers-input" name="slide_numbers[0]" id="slide_numbers_0" required>
+                    <label for="slide_numbers_0">Slide Numbers:</label>
+                    <input type="text" class="slide-numbers-input" name="slide_numbers[0]" id="slide_numbers_0"
+                        required>
 
-                <button type="button" class="remove-field">Remove</button>
+                    <button type="button" class="remove-field">Remove</button>
 
-                        </div>
-                        </div>
+                </div>
+            </div>
         </div>
         <br><br>
         <div id="slideCountResult"></div>
@@ -71,7 +74,7 @@
     </form>
 
     <script>
-        $(document).ready(function () {
+        $(document).ready(function() {
             // Add a custom validation method for unique merge_order values
             $.validator.addMethod("uniqueMergeOrder", function(value, element) {
                 // Get all existing merge_order values
@@ -89,25 +92,26 @@
             }, "Merging Order must be unique.");
 
             // Add a click event handler to elements with the .remove-field class
-            // $(".remove-field").click(function() {
-            $("#merge-pdf-form").on('click', '.remove-field', function () {
-                // alert();
+            $("#merge-pdf-form").on('click', '.remove-field', function() {
                 // Remove the parent element with the .file-detailss class
                 $(this).closest(".file-detailss").remove();
                 $('.file-details .pdf-file').each(function(index, element) {
-                    // alert(index);
-                    $(this).attr('id', 'pdf-file_' + index).attr('name','pdf_file['+ index +']').attr('id', 'pdf_file_' + index);
-                    $(this).next("label").attr("id", 'pdf_file_' + index+ '-error').attr("for", 'pdf_file_' + index);
+                    $(this).attr('id', 'pdf-file_' + index).attr('name', 'pdf_file[' + index + ']')
+                        .attr('id', 'pdf_file_' + index);
+                    $(this).next("label").attr("id", 'pdf_file_' + index + '-error').attr("for",
+                        'pdf_file_' + index);
                 });
                 $('.file-details .slide-numbers-input').each(function(index, element) {
-                    $(this).attr('id', 'slide_numbers_' + index).attr('name','slide_numbers['+ index +']').attr('id', 'slide_numbers_' + index);
-                    $(this).next("label").attr("id", 'slide_numbers_' + index+ '-error').attr("for", 'slide_numbers_' + index);
+                    $(this).attr('id', 'slide_numbers_' + index).attr('name', 'slide_numbers[' +
+                        index + ']').attr('id', 'slide_numbers_' + index);
+                    $(this).next("label").attr("id", 'slide_numbers_' + index + '-error').attr(
+                        "for", 'slide_numbers_' + index);
                 });
                 $("#merge-pdf-form").validate();
             });
 
             // Clone the template and append it when the "Add More Files" button is clicked
-            $('#add-field').click(function () {
+            $('#add-field').click(function() {
                 var clonedField = $('.file-details-template').clone();
                 clonedField.removeClass('file-details-template');
                 clonedField.addClass('file-detailss');
@@ -119,7 +123,7 @@
                 clonedField.find('.pdf-file').attr('id', 'pdf_file_' + newIndex).attr('name', 'pdf_file[' +
                     newIndex + ']');
                 clonedField.find('.merge-order-input').attr('id', 'merge_order_' + newIndex).attr('name',
-                    'merge_order[]').attr('value',newIndex);
+                    'merge_order[]').attr('value', newIndex);
                 clonedField.find('.slide-numbers-input').attr('id', 'slide_numbers_' + newIndex).attr(
                     'name', 'slide_numbers[' + newIndex + ']');
             });
@@ -192,4 +196,5 @@
         });
     </script>
 </body>
+
 </html>
